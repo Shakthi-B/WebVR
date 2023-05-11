c8ba2f608f8b67d5c689120816f2e7451e5e0eab
SHAKTHI <shakthebarane@gmail.com>
2023-05-11 12:37:26 +0530

    Animating
    Animating a Text movement

--- index.vr.js
+++ index.vr.js
@@ -7,10 +7,29 @@ import {
   Pano,
   Sphere,
   Text,
+  Animated,
   View,
 } from 'react-vr';
 
 export default class WebVR extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      animateValue: new Animated.Value(2.1)
+    }
+  }
+
+  componentDidMount(){
+    Animated.timing(
+      this.state.animateValue,
+      {
+        toValue: -0.3,
+        duration: 1000,
+        delay: 1000
+      }
+    ).start();
+  }
+
   render() {
     return (
       <View>
@@ -22,7 +41,7 @@ export default class WebVR extends React.Component {
           dimHeight = {0.40}
             texture = {asset('env.jpg')}
             style = { {color: 'white',
-                        transform: [{translate: [0,0,-2]},
+                        transform: [{translate: [-2,0,0]},
                                     {rotateX: 45},
                                     {rotateY: 45},
                                     {rotateZ: 45}],
@@ -57,6 +76,19 @@ export default class WebVR extends React.Component {
                                   {rotateZ: 45}],
           }}
         />
+
+        <Animated.Text
+        style = {{
+          backgroundColor: '#00ff00',
+          fontsize: 0.8,
+          textAlignVertical: 'center',
+          layoutOrgin: [0.5,0.5],
+          transform:[{translateY: this.state.animateValue},
+                      {translateX: 0},
+                      {translateZ: -1}]
+        }}>
+        Timing Animation
+        </Animated.Text>
 
       </View>
     );
