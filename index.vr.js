245ced6c6d875dbaddfd8101a0942f863e23ae17
SHAKTHI <shakthebarane@gmail.com>
2023-05-11 10:47:34 +0530

    Trying States with 2 different images

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,58 @@
 import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import {  AppRegistry,  asset,  Pano,  Text,  View,  VrButton,} from 'react-vr';
 
-export default class WebVR extends React.Component {
-  render() {
-    return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
-    );
+export default class Static_Demo extends React.Component 
+{
+	constructor(props)
+	{
+		super(props);
+		this.state =
+		{
+			value: "Hello",
+			imgSrc: "chess-world.jpg",
+		};
+		this.i=0;
+	}
+	start()
+	{
+		if(this.i%2==0)
+		{
+				this.setState({
+					value: "Welcome",
+					imgSrc: "env.jpg",
+				});
+			}
+		else{
+			this.setState({
+				value: "Hello",
+				imgSrc: "chess-world.jpg",
+			});
+		}
+    this.i++;
+	}
+	render() {
+		return (
+		  <View>
+			<Pano source={asset(this.state.imgSrc)}/>
+			<VrButton onClick={() => this.start()}>
+				<Text
+				  style={{
+					backgroundColor: '#777879',
+					fontSize: 0.8,
+					fontWeight: '400',
+					layoutOrigin: [0.5, 0.5],
+					paddingLeft: 0.2,
+					paddingRight: 0.2,
+					textAlign: 'center',
+					textAlignVertical: 'center',
+					transform: [{translate: [0, 0, -3]}],
+				  }}>
+				  {this.state.value}
+				</Text>
+			</VrButton>
+		  </View>
+		);
   }
 };
 
-AppRegistry.registerComponent('WebVR', () => WebVR);
+AppRegistry.registerComponent('Static_Demo', () => Static_Demo);
